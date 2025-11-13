3cc5f27330ebc4adb4a6382e7eec8b4fee639208
Chris DiCono <dicono.c@northeastern.edu>
2025-11-13 17:22:21 -0500

    note id table started

--- frontend/scripts/game-scripts/noteFretFeatures.js
+++ frontend/scripts/game-scripts/noteFretFeatures.js
@@ -1,6 +1,36 @@
-const noteFretFeatures = new Map();
+const noteFretFeatures = [
+  {
+    note: "E2",
+    string: "0",
+    fret: "0",
+    pitch: 82.49617700218863,
+    rms: 0.08659129210198177,
+    spectralCentroid: 0.19400626483986413,
+    spectralRolloff: 17.526313287589083,
+    spectralSlope: 8.600091945983722e-8,
+  },
+  {
+    note: "F2",
+    string: "0",
+    fret: "1",
+    pitch: 0,
+    rms: 0,
+    spectralCentroid: 0,
+    spectralRolloff: 0,
+    spectralSlope: 0,
+  },
+];
 
-noteFretFeatures.set((0, 0), null);
+// {
+//   note: "F#2",
+//   string: "0",
+//   fret: "2",
+//   pitch: ...,
+//   rms: ...,
+//   spectralCentroid: ...,
+//   spectralRolloff: ...,
+//   spectralSlope: ...,
+// },
 
 const mostSimilarFeatures = (features) => {
   // scan file for most similar features
